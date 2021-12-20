b79a96744343c43ed7fd9f09e887d9d27c9a2512
deepakbhattmits <deepakbhattmits@gmail.com>
2021-12-20 07:58:48 +0530

    Create beer.constants.ts

---
+++ src_/constants/beer.constants.ts
@@ -0,0 +1,9 @@
+
+/** @format */
+// beerConstants
+export const beerConstants = {
+	GET_BEERS: 'GET_BEERS',
+	GET_BEER_BY_NAME: 'GET_BEER_BY_NAME',
+	ADD_FAVORITE: 'ADD_FAVORITE',
+	REMOVE_FAVORITE: 'REMOVE_FAVORITE',
+};
