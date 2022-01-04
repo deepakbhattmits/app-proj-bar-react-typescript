2a7a0b8c66db7a615ec05acac5efa7e9306bfdd2
deepakbhattmits <deepakbhattmits@gmail.com>
2022-01-04 08:54:27 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,8 @@ import './assets/styles/_style.scss';
 import { persistStore, persistReducer } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import storage from 'redux-persist/lib/storage';
+// let environment = process.env.NODE_ENV || 'development';
+// console.log('TEST :', environment)
 
 const composeEnhancers = (window&&(window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const persistConfig = {
