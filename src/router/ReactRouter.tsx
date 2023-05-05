ac910d2e97d3b09ed94240af5bdca0d082f76edf
deepakbhattmits <deepakbhattmits@gmail.com>
2023-05-05 21:05:07 +0530

    Update ReactRouter.tsx

--- src/router/ReactRouter.tsx
+++ src/router/ReactRouter.tsx
@@ -1,44 +1,42 @@
 /** @format */
+
 import { FC, lazy, Suspense } from "react";
-import {
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
-// const ErrorPage = lazy(() => import("../reusable/ErrorPage"));
-const LoadingSpinner = lazy(() => import("../reusable/LoadingSpinner"));
-const RootLayout = lazy(() => import("../reusable/RootLayout"));
+import { HashRouter, Route, Routes } from "react-router-dom";
+
+import LoadingSpinner from "../reusable/LoadingSpinner";
+// import ErrorBoundary from '../reusable/ErrorBoundary';
+const Header = lazy(() => import("../containers/Header"));
+const Footer = lazy(() => import("../containers/Footer"));
 const MainPage = lazy(() => import("../containers/MainPage"));
 const FavoritePage = lazy(() => import("../containers/FavoritePage"));
 const NotFound = lazy(() => import("../components/NotFound"));
-
-// import LoadingSpinner from "../reusable/LoadingSpinner";
-// import RootLayout from "../reusable/RootLayout";
-// import MainPage from "../containers/MainPage";
-// import FavoritePage from "../containers/FavoritePage";
-// import NotFound from "../components/NotFound";
-
 // const ScrollToTop = lazy(() => import('../components/ScrollToTop'));
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<MainPage />} />
-
-      <Route path="/home" element={<MainPage />} />
-
-      <Route path="/favorite" element={<FavoritePage />} />
-
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
 const ReactRouter: FC = (): JSX.Element => (
   <Suspense fallback={<LoadingSpinner />}>
-    {/* <ScrollToTop> */}
-    <RouterProvider router={router} />
-    {/* </ScrollToTop> */}
+    <HashRouter>
+      {/* <ScrollToTop> */}
+      <header>
+        <Header />
+      </header>
+
+      <article>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+
+          <Route path="/home" element={<MainPage />} />
+
+          <Route path="/favorite" element={<FavoritePage />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </article>
+
+      <footer>
+        <Footer />
+      </footer>
+      {/* </ScrollToTop> */}
+    </HashRouter>
   </Suspense>
 );
 export default ReactRouter;
