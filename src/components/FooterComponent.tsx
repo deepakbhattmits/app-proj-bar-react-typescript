6c421226d7d2d97141e968cbda2285895cbc2db1
deepakbhattmits <deepakbhattmits@gmail.com>
2023-05-06 13:26:07 +0530

    Update FooterComponent.tsx
    commented for now navLinks

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,21 +1,21 @@
 /** @format */
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 //  Navigation
 const Navigation = () => (
-	<div className='ui secondary  menu footer'>
-		<div className='item'>
-			<NavLink className='ui header text-capitalize' to='/'>
+  <div className="ui secondary  menu footer">
+    <div className="item">
+      {/* <NavLink className='ui header text-capitalize' to='/'>
 				Project Name : ( footer )
-			</NavLink>
-		</div>
-		<div className='right menu'>
-			<NavLink className='item text-uppercase' to='/home'>
-				home
-			</NavLink>
-			<NavLink className='item text-uppercase' to='/favorite'>
-				favorite
-			</NavLink>
-		</div>
-	</div>
+			</NavLink> */}
+    </div>
+    <div className="right menu">
+      <NavLink className="item text-uppercase" to="/home">
+        home
+      </NavLink>
+      <NavLink className="item text-uppercase" to="/favorite">
+        favorite
+      </NavLink>
+    </div>
+  </div>
 );
 export default Navigation;
