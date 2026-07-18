a017cfe39ee54e09251a1634baef4c9f294fc995
Deepak Bhatt <deepakbhattmits@gmail.com>
2026-07-18 15:37:39 +0530

    Refactor

--- src/constants/beer.constants.ts
+++
@@ -1,9 +0,0 @@
-
-/** @format */
-// beerConstants
-export const beerConstants = {
-	GET_BEERS: 'GET_BEERS',
-	GET_BEER_BY_NAME: 'GET_BEER_BY_NAME',
-	ADD_FAVORITE: 'ADD_FAVORITE',
-	REMOVE_FAVORITE: 'REMOVE_FAVORITE',
-};
